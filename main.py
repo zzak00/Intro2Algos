e701bc69c0919a6eac95378985efcacc89ec34e9
zzak00 <abderrazzak.bajjou@um6p.ma>
2021-07-28 18:04:35 +0100

    bubble sort

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import peakFinder as pf
 import numpy as np
 ''' A=[1,3,4,1,2]
 print('Insertion Sort')
-print(sp.insertion_sort(A))
+print(sp.insertionSort(A))
 print('Selection Sort')
-print(sp.selection_sort(A))
+print(sp.selectionSort(A))
 #A=[1,3,5,2,4]
 #print(sp.merge(A,0,3,5))
 print('Merge Sort')
-print(sp.merge_sort(A,0,5))'''
+print(sp.mergeSort(A,0,5))'''
 
 '''
 problemMatrix = np.array([
