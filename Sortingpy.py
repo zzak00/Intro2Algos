e03df01531c5938076674e480a921e92a66e127e
zzak00 <abderrazzak.bajjou@um6p.ma>
2021-07-30 20:20:13 +0100

    mergeSort

--- Sortingpy.py
+++ Sortingpy.py
@@ -26,20 +26,21 @@ def selectionSort(A):
 def merge(A,p,q,r):
     # p: index start subarray 1
     # q: index start subarray 2
-    L=A[p:q]
-    R=A[q:r]
+    # r: ending index subarray 2 (included)
+    L=A[p:q+1]
+    R=A[q+1:r+1]
     L.append(float('inf'))
     R.append(float('inf'))
     i=0
     j=0
-    for k in range(p,r):
+    for k in range(p,r+1):
         if L[i]<=R[j]:
             A[k]=L[i]
             i=i+1
         else:
             A[k]=R[j]
             j=j+1
-    return A
+    
 
 def mergeSort(A,p,r):
     if p<r :
@@ -47,7 +48,6 @@ def mergeSort(A,p,r):
         mergeSort(A,p,q)
         mergeSort(A,q+1,r)
         merge(A,p,q,r)
-    return A
 
 def bubbleSort(A): 
     n = len(A) 
--- main.py
+++ main.py
@@ -6,12 +6,14 @@ print('Insertion Sort')
 print(sp.insertionSort(A))
 print('Selection Sort')
 print(sp.selectionSort(A))
-#A=[1,3,5,2,4]
-#print(sp.merge(A,0,3,5))
+
 print('Merge Sort')
 print(sp.mergeSort(A,0,5))'''
-
+A=[3,2,8,6,1]
+sp.mergeSort(A,0,4)
+print(A)
 '''
+
 problemMatrix = np.array([
 	[ 4,  5,  6,  7,  8,  7,  6,  5,  4,  3,  2],
 	[ 5,  6,  7,  8,  9,  8,  7,  6,  5,  4,  3],
@@ -31,5 +33,5 @@ print(pf.peakfinder_2D(problemMatrix,0,m))'''
 #print(pf.peakfinder_1D(a,0,2))
 #A=[1,3,4,1,2]
 #print(sp.bubbleSort(A))
-A=[10,20,25,6,12,15,4,16]
-print(sp.HeapSort(A))
+#A=[10,20,25,6,12,15,4,16]
+#print(sp.HeapSort(A))
