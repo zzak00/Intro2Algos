6f3d9676c9a19b44b4090b817d71256b8fa230cb
zzak00 <abderrazzak.bajjou@um6p.ma>
2021-07-30 23:38:24 +0100

    d&c

---
+++ divide&conquer.py
@@ -0,0 +1,47 @@
+# Inversions
+def inversionsBF(arr):
+    num_inversions = 0
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if arr[i] > arr[j]:
+                num_inversions += 1
+
+    return num_inversions
+
+def mergeInv(A,p,q,r):
+    global count
+    # p: index start subarray 1
+    # q: index start subarray 2
+    # r: ending index subarray 2 (included)
+    # A=[3,2,8,6,1]
+    # mergeSort(A,0,4)
+    L=A[p:q+1]
+    R=A[q+1:r+1]
+    L.append(float('inf'))
+    R.append(float('inf'))
+    i=j=0
+    for k in range(p,r+1):
+        if L[i]>R[j]:
+            A[k]=R[j]
+            j=j+1
+            count+=len(L)-1
+        else:
+            A[k]=L.pop(0)
+
+def count_inversions(A,p,r,count):
+    if p<r :
+        q=(p+r)//2
+        count_inversions(A,p,q,count)
+        count_inversions(A,q+1,r,count)
+        mergeInv(A,p,q,r)
+
+def main():
+    a=[1,5,4,8,10,2,6,9,12,11,3,7]
+    print("number of inversions brute force = ", inversionsBF(a))
+    count_inversions(a,0,len(a)-1,count)
+    print("number of inversions divide & conquer ",count)
+
+if __name__ == "__main__":
+    count=0
+    main()
