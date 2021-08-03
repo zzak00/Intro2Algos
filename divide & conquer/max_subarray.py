df9e215ae72cb6fa81d0013fc65d199036a91838
zzak00 <abderrazzak.bajjou@um6p.ma>
2021-08-03 23:08:13 +0100

    max subarr

--- divide & conquer/max_subarray.py
+++ divide & conquer/max_subarray.py
@@ -15,4 +15,11 @@ def find_max_crossing_subarray(A,low,mid,high):
         if sum > right_sum :
             right_sum = sum
             max_right = j
-    return max_left,max_right,left_sum+right_sum
+    return max_left+1,max_right+1,left_sum+right_sum
+
+def main():
+    a=[13,-3,-25,20,-3,-16,-23,18,20,-7,12,-5,-22,-15,-4,7]
+    print(find_max_crossing_subarray(a,0,9,len(a)-1))
+
+if __name__ == "__main__":
+    main()
