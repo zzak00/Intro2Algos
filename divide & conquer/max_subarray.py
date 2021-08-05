965d8e46db27330dfdd103c3c83c64c659a46c19
zzak00 <abderrazzak.bajjou@um6p.ma>
2021-08-05 22:30:43 +0100

    max subarray linear time

--- divide & conquer/max_subarray.py
+++ divide & conquer/max_subarray.py
@@ -36,9 +36,59 @@ def find_maximum_subarray(A,low,high):
             else:
                 return cross_low,cross_high,cross_sum
 
-def main():
-    a=[13,3,25,-20,-3,-16,-23,18,20,-7,-12,-5,-22,-15,-4,7]
-    print(find_maximum_subarray(a,0,len(a)-1))
 
+def max_suba_l(a):
+    l = r = 0
+    max_sum = a[0]
+    sum_now = a[0]
+    for j in range(0,len(a)-2):
+        print(sum_now)
+        sum_now+=a[j+1]
+        if sum_now >= max_sum :
+                r= j+1
+                max_sum=sum_now
+        else :
+            if a[j] + a[j+1] >= max_sum :
+                l=j
+                r=j+1
+                max_sum=a[j]+a[j+1]
+                sum_now=a[j]+a[j+1]
+    return (l,r,max_sum)
+
+from sys import maxsize
+ 
+# Function to find the maximum contiguous subarray
+# and print its starting and end index
+def maxSubArraySum(a,size):
+ 
+    max_so_far = -maxsize - 1
+    max_ending_here = 0
+    start = 0
+    end = 0
+    s = 0
+ 
+    for i in range(0,size):
+ 
+        max_ending_here += a[i]
+ 
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+            start = s
+            end = i
+ 
+        if max_ending_here < 0:
+            max_ending_here = 0
+            s = i+1
+ 
+    print ("Maximum contiguous sum is %d"%(max_so_far))
+    print ("Starting Index %d"%(start))
+    print ("Ending Index %d"%(end))
+ 
+# Driver program to test maxSubArraySum
+
+def main():
+    a=[13,3,-25,-20,8,16,-23,18,20,-7,-12,-5,-22,-15,-4,7]
+    #print(max_suba_l(a))
+    maxSubArraySum(a,len(a))
 if __name__ == "__main__":
     main()
